2fe9d3980b009b6bf219611b3b4f4c5fa437d4c1
DanMobile <daniel.shenc@gmail.com>
2017-03-23 17:44:38 -0400

    Create my-data.ts

---
+++ PDF Display Module/my-data.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+export interface ReportParameter {
+  name: string;
+  parameterID: number;
+  value: string;
+}
+
+export interface ReportPost {
+  reportId: number;
+  parameters: Array<ReportParameter>;
+  name: string;
+}
+
+@Injectable()
+export class TMWData {
+
+  constructor(public http: Http, public user: UserData) { }
+  setHeaders(){
+    // the head info you wanna pass in http post
+  }
+  getReport(reportPost: ReportPost) {
+      var url = this.myURL + "Report?reportId=" + reportPost.reportId;
+      let headers = this.setHeaders();
+      let postData = JSON.stringify(reportPost.parameters);
+      let options = new RequestOptions({ headers: headers }); // Create a request option
+      // use promise to retrieve data from server async
+      return new Promise(resolve => {
+        this.http.post(url, postData, options)
+          .map(res => res.json())
+          .subscribe(data => {
+            resolve(data);
+          },
+          err => {
+            resolve(err);
+          })
+      });
+   }
+ }
